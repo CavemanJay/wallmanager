2f5e04dadd7b12a6f6ba6c6679eb2ac7e0b05ecf
Jay <j>
2022-10-22 17:52:46 -0700

    Add config file Add anyhow error handling Fix clippy lint suggestions

--- src/extensions.rs
+++ src/extensions.rs
@@ -2,9 +2,8 @@ pub trait LineSplitter {
     fn split_lines(&self) -> Vec<String>;
 }
 
-impl LineSplitter for &str
-{
+impl LineSplitter for &str {
     fn split_lines(&self) -> Vec<String> {
-        self.split("\n").map(ToString::to_string).collect()
+        self.split('\n').map(ToString::to_string).collect()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::{Context, Result};
 use extensions::LineSplitter;
 use inquire::Editor;
 use log::{debug, info, LevelFilter};
@@ -6,8 +7,10 @@ use rand::seq::SliceRandom;
 use rand::{self, thread_rng};
 use std::collections::HashSet;
 use std::ffi::OsStr;
+use std::io::{Read, Write};
 use std::path::Path;
 use std::process::Stdio;
+use std::{env, fs};
 use std::{
     io,
     process::Command,
@@ -15,17 +18,21 @@ use std::{
     thread,
     time::Duration,
 };
-use wallpaper;
+
 mod extensions;
 
 const INTERVAL_SECS: u8 = 10;
 #[cfg(windows)]
 const BG_ROOT: &str = "S:/Backgrounds";
 
+const CFG_FILE: &str = "cfg.json";
+
 #[cfg(windows)]
 const EDITOR_CMD: &str = r"C:\WINDOWS\gvim.bat";
 #[cfg(not(windows))]
 const EDITOR_CMD: &str = "vim";
+
+type Config = Vec<String>;
 
 enum InputAction {
     PrintCurrent,
@@ -34,19 +41,20 @@ enum InputAction {
     ChooseFolders(String),
     ReloadWallpapers,
     AppendSelection(String),
-    Nop
+    Nop,
 }
 
-fn main() -> io::Result<()> {
+fn main() -> Result<()> {
     env_logger::builder()
         .filter_level(LevelFilter::Debug)
         .init();
 
+    let cfg = read_cfg().with_context(|| "Failed to read initial config file")?;
+
     let files = Arc::new(Mutex::new(load_wallpapers()?));
     let thread_files = files.clone();
     let watcher_files = files.clone();
-    // let chosen_folders = Arc::new(Mutex::new(HashSet::from([BG_ROOT.to_string()])));
-    let chosen_folders = Arc::new(Mutex::new(Vec::from([BG_ROOT.to_string()])));
+    let chosen_folders = Arc::new(Mutex::new(cfg));
     let t_chosen_folders = chosen_folders.clone();
 
     let mut watcher =
@@ -100,7 +108,7 @@ fn main() -> io::Result<()> {
     });
 
     let update_chosen_folders = |folders: Vec<String>, append: bool| {
-        let folders = if folders.len() == 0 || (folders.len() == 1 && folders[0] == "") {
+        let folders = if folders.is_empty() || (folders.len() == 1 && folders[0].is_empty()) {
             vec![BG_ROOT.to_string()]
         } else {
             folders
@@ -117,6 +125,7 @@ fn main() -> io::Result<()> {
         };
 
         info!("New selection: {:?}", new_selection);
+        write_cfg(&new_selection).expect("Could not update config file");
         *selected = new_selection;
     };
 
@@ -128,7 +137,7 @@ fn main() -> io::Result<()> {
                 .with_predefined_text(&folders.join("\n"))
                 .prompt_skippable()
                 .unwrap()
-                .unwrap_or("".to_string())
+                .unwrap_or_default()
                 .as_str()
                 .trim()
                 .split_lines()
@@ -195,8 +204,8 @@ fn get_folders(input: &str) -> Vec<String> {
 
     let output = String::from_utf8_lossy(&fzf.stdout)
         .trim()
-        .split("\n")
-        .map(|s| s.replace("\\", "/"))
+        .split('\n')
+        .map(|s| s.replace('\\', "/"))
         .collect::<Vec<_>>();
 
     match output.len() {
@@ -208,7 +217,7 @@ fn get_folders(input: &str) -> Vec<String> {
                 // .with_editor_command(OsStr::new(r"C:\WINDOWS\vim.bat"))
                 .prompt_skippable()
                 .unwrap()
-                .unwrap_or("".to_string())
+                .unwrap_or_default()
                 .as_str()
                 .split_lines();
             received
@@ -216,7 +225,7 @@ fn get_folders(input: &str) -> Vec<String> {
     }
 }
 
-fn load_wallpapers() -> io::Result<HashSet<String>> {
+fn load_wallpapers() -> Result<HashSet<String>> {
     let output = Command::new("fd")
         .args(["-t", "f", "jpg|png", BG_ROOT])
         .output()?;
@@ -225,7 +234,7 @@ fn load_wallpapers() -> io::Result<HashSet<String>> {
     let out_str = String::from_utf8_lossy(&out);
     let mut files = out_str
         .split('\n')
-        .map(|s| s.replace("\\", "/"))
+        .map(|s| s.replace('\\', "/"))
         .collect::<HashSet<_>>();
 
     files.reserve(100);
@@ -233,4 +242,44 @@ fn load_wallpapers() -> io::Result<HashSet<String>> {
     debug!("Loaded {} files", files.len());
 
     Ok(files)
+}
+
+fn read_cfg() -> Result<Vec<String>> {
+    let path = env::current_dir()
+        .context("Failed to retrieve current directory.")?
+        .join(CFG_FILE);
+
+    if !path.exists() {
+        debug!("Creating initial config file");
+        let default = vec![BG_ROOT.into()];
+        write_cfg(&default)?;
+        return Ok(default);
+    }
+
+    let mut contents = String::new();
+    let mut cfg_file = fs::File::options()
+        .read(true)
+        .open(path)
+        .context("Failed to open config file for reading")?;
+
+    cfg_file
+        .read_to_string(&mut contents)
+        .context("Failed to read file contents to string")?;
+
+    let cfg = serde_json::from_str(&contents).context("Failed to parse config file")?;
+    Ok(cfg)
+}
+
+fn write_cfg(val: &Config) -> Result<()> {
+    let mut cfg_file = fs::File::options()
+        .write(true)
+        .create(true)
+        .open(CFG_FILE)
+        .context("Failed to open config file for writing")?;
+
+    let stringified = serde_json::to_string(&val).context("Failed to stringify config value")?;
+    cfg_file
+        .write(stringified.as_bytes())
+        .context("Failed to write config file")?;
+    Ok(())
 }
